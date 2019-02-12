e4066c3558483ca0115a07b8e4f46bfb6cf877d2
alexeykomov <alexeykofficial@gmail.com>
2019-02-13 00:23:26 +0300

    add: iOS scaffold

---
+++ ios/RNRnmentoringprogramAsyncStorage.swift
@@ -0,0 +1,33 @@
+//
+//  RNRnmentoringprogramAsyncStorage.swift
+//  RNRnmentoringprogramAsyncStorage
+//
+//  Created by Alex K on 2/13/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+
+@objc(RNRnmentoringprogramAsyncStorage)
+class RNRnmentoringprogramAsyncStorage: NSObject {
+    
+    @objc(getItem)
+    func getItem() -> String {
+        
+    }
+    
+    @objc(setItem:item)
+    func setItem(item: String) {
+        
+    }
+    
+    @objc(setMultipleItems:items)
+    func setMultipleItems(items: [String]) {
+        
+    }
+    
+    @objc(getMultipleItems)
+    func getMultipleItems() -> [String] {
+        
+    }
+}
